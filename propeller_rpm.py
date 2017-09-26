687950777e9dfabc01c2273798ec2fc08a6c6014
reoliveira <oliveira.reid@gmail.com>
2017-09-25 23:24:10 -0700

    less crowded options for now + centre of colour calculation

--- propeller_rpm.py
+++ propeller_rpm.py
@@ -1,35 +1,48 @@
 import numpy as np
 import cv2
 import argparse
-np.set_printoptions(threshold=np.nan)
 
-def get_color_bounds(color) :
-    if color == "red":
-        return ([17, 15, 100], [50, 56, 200])
+def get_color_bounds():
+    return ([10, 10, 100], [80, 80, 200])
+
+def weigh_pixels(pixels) :
+    totalx = 0
+    totaly = 0
+    count = 0
+
+    if pixels.size <= 0: return (0,0)
+
+    for pixel in np.nditer(pixels, flags = ['external_loop'], order = 'C'):
+        totalx += pixel[0]
+        totaly += pixel[1]
+        count += 1
+
+    return (totalx/count, totaly/count)
 
 # retrieve arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-v", "--video", help = "path to the video")
-ap.add_argument("-c", "--color", help = "the color to filter: red")
-ap.add_argument("-o", "--original", help = "display original")
 args = vars(ap.parse_args())
 
 cap = cv2.VideoCapture(args["video"])
 
-boundaries = get_color_bounds(args["color"])
+boundaries = get_color_bounds()
 lower = np.array(boundaries[0], dtype = "uint8")
 upper = np.array(boundaries[1], dtype = "uint8")
 
 while(cap.isOpened()):
     ret, frame = cap.read()
 
+    # split the target color from the rest of the image
     binary = cv2.inRange(frame, lower, upper)
+    # locations of all target color pixels
+    targets = np.transpose(np.where(binary>0))
+    # "centre of mass" of the targets
+    centre = weigh_pixels(targets)
+    print centre
 
-    if args["original"] == "true":
-        cv2.imshow('frame', frame)
-    else:
-        cv2.imshow('frame', binary)
-    if cv2.waitKey(10) & 0xFF == ord('q'):
+    cv2.imshow('frame', binary)
+    if cv2.waitKey(5) & 0xFF == ord('q'):
         break
 
 cap.release()
